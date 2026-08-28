7a204d8eb3b48dc31d1712333406224b362c3067
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-28 10:39:52 +0530

    all changes done

--- modules/endpoints/variables.tf
+++ modules/endpoints/variables.tf
@@ -10,18 +10,3 @@ variable "region" {
   type = string
 }
 
-variable "vpc_id" {
-  type = string
-}
-
-variable "route_table_ids" {
-  type = list(string)
-}
-
-variable "subnet_ids" {
-  type = list(string)
-}
-
-variable "region" {
-  type = string
-}
