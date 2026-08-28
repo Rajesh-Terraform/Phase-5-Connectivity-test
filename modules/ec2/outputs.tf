44b87122e6b971d6f29b375787189c0b3d119305
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-08-28 10:56:13 +0530

    all changes done

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,14 +1,9 @@
 output "instance_id" {
-  description = "EC2 instance ID"
+  description = "ID of the EC2 instance"
   value       = aws_instance.this.id
 }
 
 output "private_ip" {
-  description = "EC2 private IP"
+  description = "Private IP address of the EC2 instance"
   value       = aws_instance.this.private_ip
-}
-
-output "security_group_id" {
-  description = "EC2 security group ID"
-  value       = aws_security_group.ec2.id
 }
